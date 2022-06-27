860b973fc92073b6fc02b19d3ec16065e6030f46
rmolines <rafaelmismael@gmail.com>
2022-06-27 13:38:39 -0300

    asdfasdf

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,9 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
-  });
+  res.status(200).json({ people: req.body });
 }
