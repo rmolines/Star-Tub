8cbf22b68cc068242ccba771aa5ff6fab07256bb
rmolines <rafaelmismael@gmail.com>
2022-06-27 11:31:10 -0300

    2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,41 @@
-import { useUser } from '@auth0/nextjs-auth0';
+import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { doc, getDoc, getFirestore } from 'firebase/firestore';
+import { app } from 'firebaseConfig';
 import router from 'next/router';
 import { Fragment, useEffect } from 'react';
 
-export default function Index() {
+import { useUserInfo } from '@/context/UserInfoContext';
+
+export default withPageAuthRequired(function Index() {
   const { user } = useUser();
-  // const { setUserInfo } = useUserInfo();
+  const { setUserInfo } = useUserInfo();
 
   const getUser = async () => {
-    // const userInfo = await getDoc(
-    //   doc(
-    //     getFirestore(app),
-    //     'users',
-    //     typeof user?.sub === 'string' ? user?.sub : ''
-    //   )
-    // );
-    router.push('/api/auth/login');
+    const userInfo = await getDoc(
+      doc(
+        getFirestore(app),
+        'users',
+        typeof user?.sub === 'string' ? user?.sub : ''
+      )
+    );
 
-    // if (userInfo === undefined || !userInfo.exists()) {
-    //   router.push('/api/auth/login');
-    // } else {
-    //   setUserInfo(userInfo);
-    //   if (userInfo.data().userType === 'founder') {
-    //     router.push('/founder/questions/');
-    //   } else {
-    //     router.push('/investor/companies/');
-    //   }
-    // }
+    if (userInfo === undefined || !userInfo.exists()) {
+      router.push('completeRegistration');
+    } else {
+      setUserInfo(userInfo);
+      if (userInfo.data().userType === 'founder') {
+        router.push('/founder/questions/');
+      } else {
+        router.push('/investor/companies/');
+      }
+    }
   };
 
   useEffect(() => {
     if (user) {
       getUser();
     }
-    getUser();
-  }, []);
+  }, [user]);
 
   return <Fragment />;
-}
+});
