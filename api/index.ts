1962646a9ac92a8c71825ca9e176daa3eca8a4a5
rmolines <rafaelmismael@gmail.com>
2022-06-27 13:05:31 -0300

    agora vai??

--- api/index.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
-  });
-}
